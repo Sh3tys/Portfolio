24891084c27c45f88a1fa6cd2694de50355d0d37
Sh3tys <titouanparzival@gmail.com>
2025-10-14 10:01:59 +0200

    Add loading for limit bug chargement

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  loader.style.opacity = 0;
+  setTimeout(() => loader.style.display = 'none', 500);
+});
+
+
 // ========================================
 // INITIALISATION AU CHARGEMENT
 // ========================================
